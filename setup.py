14544293e8d0e1028da05a8f9f57d18209a3d088
muathendirangu <muathe.ndirangu@gmail.com>
2024-02-05 16:15:22 +0300

    update the package name

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='reconciler',
+    name='aminimalreconciler',
     version='0.1.0',
     description='reconciler',
     url='https://github.com/muathendirangu/reconcile',
     author='muathendirangu',
     author_email='muathe.ndirangu@gmail.com',
     license='MIT',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'reconciler=reconciler.reconciler:main'
