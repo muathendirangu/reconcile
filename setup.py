762c1fb2c2f5969024a426e20a9ccaed749beb95
muathendirangu <muathe.ndirangu@gmail.com>
2024-02-05 15:50:39 +0300

    fix error on description content

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='reconciler',
     version='0.1.0',
-    description='Identify discrepancies, missing rows among two CSV files',
+    description='reconciler',
     url='https://github.com/muathendirangu/reconcile',
     author='muathe ndirangu',
     author_email='muathe.ndirangu@gmail.com',
