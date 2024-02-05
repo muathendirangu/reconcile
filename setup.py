3ec6bf024c08c425f79f8046d5ef61e3804b97ef
muathendirangu <muathe.ndirangu@gmail.com>
2024-02-05 17:32:33 +0300

    bump up the package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aminimalreconciler',
-    version='0.1.0',
+    version='0.1.1',
     description='reconciler',
     url='https://github.com/muathendirangu/reconcile',
     author='muathendirangu',
